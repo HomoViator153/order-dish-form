f4288920403e4caabfeeaf5b5a479753b868a6d4
Antoni Klebba <Antoni.Klebba@gmail.com>
2022-03-05 09:28:28 +0100

    Handle input changes in BasicForm

--- src/components/BasicForm.jsx
+++ src/components/BasicForm.jsx
@@ -4,20 +4,45 @@ const BasicForm = (props) => {
   const { name, type, preparation_time } = props.formState;
   const { isDishTypeSet, setIsDishTypeSet, setFormState } = props;
 
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
     <>
       <label htmlFor="name">Dish name: </label>
-      <input type="text" value={name} name="name" id="name" required autofocus />
+      <input
+        type="text"
+        value={name}
+        onChange={handleOnChange}
+        name="name"
+        id="name"
+        required
+        autoFocus
+      />
       <label htmlFor="preparation_time">Preparation time: </label>
       <input
         type="time"
         value={preparation_time}
+        onChange={handleOnChange}
         name="preparation_time"
         id="preparation_time"
         step="2"
       />
       <label htmlFor="type">Dish type: </label>
-      <select name="type" value={type} id="type" required>
+      <select
+        name="type"
+        value={type}
+        onChange={(e) => {
+          handleOnChange(e);
+          if (isDishTypeSet) setIsDishTypeSet(true);
+        }}
+        id="type"
+        required>
         <option hidden disabled value="">
           -- Choose dish type --
         </option>
